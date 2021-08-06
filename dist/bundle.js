485cdbbaa88bd8518ac21ea26dc559cebfd08312
Eric Nantes <ericnantes18@gmail.com>
2021-08-06 19:15:36 -0400

    Babel configs

---
+++ dist/bundle.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _jsxRuntime = require("react/jsx-runtime");
+
+function App() {
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("h1", {
+    children: "Hello World"
+  });
+}
+
+var _default = App;
+exports["default"] = _default;
