3ee87892ced92dfc840f3ca1b03b6992c836bb50
Eric Nantes <ericnantes18@gmail.com>
2021-08-10 18:20:55 -0400

    First react component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="#" target="_blank">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="#" target="_blank">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="#" target="_blank">Acessar repositório</a>
+                </li>
+            </ul>
+        </section>
+    );
+}
