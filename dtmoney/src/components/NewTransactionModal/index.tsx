e24b816b6feead04f9cc0c0e4701cf005c45ba74
Eric Nantes <ericnantes18@gmail.com>
2021-12-15 17:55:26 -0400

    transaction insertion done

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -22,15 +22,21 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
   const [type, setType] = useState('deposit');
   const [category, setCategory] = useState('');
 
-  function handleSubmitNewTransaction (event: FormEvent) {
+  async function handleSubmitNewTransaction (event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       type,
       category
     })
+
+    setTitle('');
+    setAmount(0);
+    setType('');
+    setCategory('');
+    onRequestClose();
   }
 
   return(
